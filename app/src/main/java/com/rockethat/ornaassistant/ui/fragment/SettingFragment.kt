348b8a8ea15d16eea6d12d11b596de36cbb04a8e
lloir <github@0x3d.uk>
2023-12-02 16:49:19 +0000

    Add Dark / Light mode switcher

--- app/src/main/java/com/rockethat/ornaassistant/ui/fragment/SettingFragment.kt
+++ app/src/main/java/com/rockethat/ornaassistant/ui/fragment/SettingFragment.kt
@@ -1,22 +1,37 @@
 package com.rockethat.ornaassistant.ui.fragment
 
+import android.content.SharedPreferences
 import android.os.Bundle
-import com.rockethat.ornaassistant.R
-
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
-import android.content.SharedPreferences
-
+import com.rockethat.ornaassistant.R
 
 class SettingFragment : PreferenceFragmentCompat() {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.main_preference, rootKey)
 
+        // Set a default value for a specific preference
         context?.let { nonNullContext ->
             val prefs = PreferenceManager.getDefaultSharedPreferences(nonNullContext)
             val editor: SharedPreferences.Editor = prefs.edit()
             editor.putBoolean("PREF_NAME", false)
             editor.commit()
+        }
+
+        // Listen for changes in the theme preference
+        findPreference<ListPreference>("theme_preference")?.setOnPreferenceChangeListener { _, newValue ->
+            applyTheme(newValue as String)
+            true
+        }
+    }
+    private fun applyTheme(themeValue: String) {
+        when (themeValue) {
+            "dark" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            "light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            "device" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         }
     }
 }
