ceac8dad939ff2ef8018c7f777a7e966089c3d58
THIS IS MY NAME <github@0x3d.uk>
2023-12-08 16:27:46 +0000

    Persistent notif

--- app/src/main/java/com/rockethat/ornaassistant/MainActivity.kt
+++ app/src/main/java/com/rockethat/ornaassistant/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.rockethat.ornaassistant
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
 import android.view.Menu
@@ -8,18 +13,26 @@ import android.view.MenuItem
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.ComposeView
+import androidx.core.app.NotificationCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.rockethat.ornaassistant.ui.fragment.FragmentAdapter
 import com.rockethat.ornaassistant.ui.fragment.MainFragment
-
 
 @RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
     private lateinit var tableLayout: TabLayout
     private lateinit var pager: ViewPager2
     private lateinit var adapter: FragmentAdapter
-    private val TAG = "OrnaMainActivity"
+    private val NOTIFICATION_ID = 1234
+    private val CHANNEL_ID = "persistent_notification_channel"
+
+    private val notificationReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val enabled = intent?.getBooleanExtra("enabled", false) ?: false
+            handlePersistentNotificationPreference(enabled)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +41,11 @@ class MainActivity : AppCompatActivity() {
         initializeViews()
         setupTabLayout()
         setupComposeView()
+        createNotificationChannel()
+
+        // Register the broadcast receiver
+        val filter = IntentFilter("com.rockethat.ornaassistant.UPDATE_NOTIFICATION")
+        registerReceiver(notificationReceiver, filter)
     }
 
     private fun initializeViews() {
@@ -42,6 +60,7 @@ class MainActivity : AppCompatActivity() {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 pager.currentItem = when (tab.text) {
                     "Main" -> 0
+                    // Add cases for other tabs if needed
                     else -> 0
                 }
                 updateMainFragment()
@@ -78,12 +97,47 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.item_preference) {
-            startActivity(Intent(this, SettingsActivity::class.java))
-            return true
+        return when (item.itemId) {
+            R.id.item_preference -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
-        return super.onOptionsItemSelected(item)
     }
 
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "Persistent Notification"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, channelName, importance).apply {
+                description = "Channel for persistent notification"
+            }
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
 
+    fun handlePersistentNotificationPreference(enabled: Boolean) {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (enabled) {
+            val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("App is Running")
+                .setContentText("Tap to open.")
+                .setSmallIcon(R.drawable.ic_notification) // Replace with your app's notification icon
+                .setOngoing(true)
+                .build()
+
+            notificationManager.notify(NOTIFICATION_ID, notification)
+        } else {
+            notificationManager.cancel(NOTIFICATION_ID)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Unregister the broadcast receiver
+        unregisterReceiver(notificationReceiver)
+    }
 }
